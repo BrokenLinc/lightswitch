7434c8968941d14963bac8663315374297c3241a
Lincoln Anderson <landerson@352inc.com>
2016-03-27 19:17:20 -0400

    Hiding words.

--- website/page.js
+++ website/page.js
@@ -3,6 +3,6 @@ var messageNode = document.getElementById('message');
 // Listen for the lightswitch and update the page
 document.addEventListener('lightswitch', function(e){
 	var isLightOn = e.detail;
-	messageNode.textContent = isLightOn? 'Light':'Darkness';
+	//messageNode.textContent = isLightOn? 'Light':'Darkness';
 	document.body.className = 'loaded ' + (isLightOn? 'light-on':'');
 });
