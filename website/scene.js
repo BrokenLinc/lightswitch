e318de7665550118b0d87af9c3cf38ee31e18aeb
Lincoln Anderson <landerson@352inc.com>
2016-03-27 19:03:50 -0400

    Commented to barebones scene.

--- website/scene.js
+++ website/scene.js
@@ -2,10 +2,10 @@ var scene = new THREE.Scene();
 
 // Add camera
 var camera = new THREE.PerspectiveCamera(
-	30, // field of view
-	window.innerWidth / window.innerHeight, // aspect ratio
-	0.1, // near
-	1000 // far
+	// 30, // field of view
+	// window.innerWidth / window.innerHeight, // aspect ratio
+	// 0.1, // near
+	// 1000 // far
 );
 camera.position.z = 5;
 
@@ -13,60 +13,60 @@ camera.position.z = 5;
 var renderer = new THREE.WebGLRenderer({ alpha: true });
 document.body.appendChild( renderer.domElement );
 
-// Keep camera and renderer the same size as the viewport
-function fitRendererToWindow() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-}
-fitRendererToWindow();
-window.addEventListener('resize', fitRendererToWindow);
+// // Keep camera and renderer the same size as the viewport
+// function fitRendererToWindow() {
+// 	camera.aspect = window.innerWidth / window.innerHeight;
+// 	camera.updateProjectionMatrix();
+// 	renderer.setSize( window.innerWidth, window.innerHeight );
+// }
+// fitRendererToWindow();
+// window.addEventListener('resize', fitRendererToWindow);
 
-// Add geometric shape
-var geometry = new THREE.DodecahedronGeometry();
-var material = new THREE.MeshPhongMaterial( { 
-	color: 0xffffff, // white
-	shading: THREE.FlatShading // keep edges sharp
-} );
-var shape = new THREE.Mesh( geometry, material );
-scene.add( shape );
+// // Add geometric shape
+// var geometry = new THREE.DodecahedronGeometry();
+// var material = new THREE.MeshPhongMaterial( { 
+// 	color: 0xffffff, // white
+// 	shading: THREE.FlatShading // keep edges sharp
+// } );
+// var shape = new THREE.Mesh( geometry, material );
+// scene.add( shape );
 
-// Add ambient light
-// (off-white)
-var ambientLight = new THREE.AmbientLight( 0xdddddd, 1 );
-scene.add( ambientLight );
+// // Add ambient light
+// // (off-white)
+// var ambientLight = new THREE.AmbientLight( 0xdddddd, 1 );
+// scene.add( ambientLight );
 
-// Add directional light
-// (a bit brighter)
-var directionalLight = new THREE.DirectionalLight( 0x1c1c1c, 1 );
-directionalLight.position.set( 0, 1, 0.5 );
-scene.add( directionalLight );
+// // Add directional light
+// // (a bit brighter)
+// var directionalLight = new THREE.DirectionalLight( 0x1c1c1c, 1 );
+// directionalLight.position.set( 0, 1, 0.5 );
+// scene.add( directionalLight );
 
-// Add wireframe to shape
-wireframe = new THREE.EdgesHelper( shape, 0xff8800 ); // dark grey lines
-wireframe.material.linewidth = 2;
-scene.add( wireframe );
+// // Add wireframe to shape
+// wireframe = new THREE.EdgesHelper( shape, 0xff8800 ); // dark grey lines
+// wireframe.material.linewidth = 2;
+// scene.add( wireframe );
 
-// Listen for lightswitch event and update scene
-document.addEventListener('lightswitch', function(e){
-	setLightIsOn(e.detail);
-});
-function setLightIsOn(isLightOn) {
-	if(isLightOn) {
-		renderer.setClearColor( 0xffffff, 0); // white
-		directionalLight.intensity = ambientLight.intensity = 1; // full brightness
-		wireframe.visible = false;
-	} else {
-		renderer.setClearColor( 0x000000, 0); // black
-		directionalLight.intensity = ambientLight.intensity = 0.08; // 10% brightness
-		wireframe.visible = true;
-	}
-}
+// // Listen for lightswitch event and update scene
+// document.addEventListener('lightswitch', function(e){
+// 	setLightIsOn(e.detail);
+// });
+// function setLightIsOn(isLightOn) {
+// 	if(isLightOn) {
+// 		renderer.setClearColor( 0xffffff, 0); // white
+// 		directionalLight.intensity = ambientLight.intensity = 1; // full brightness
+// 		wireframe.visible = false;
+// 	} else {
+// 		renderer.setClearColor( 0x000000, 0); // black
+// 		directionalLight.intensity = ambientLight.intensity = 0.08; // 10% brightness
+// 		wireframe.visible = true;
+// 	}
+// }
 
 // Render loop and animate spinning of shape
 function render() {
-	shape.rotation.x += 0.01;
-	shape.rotation.y += 0.01;
+	// shape.rotation.x += 0.01;
+	// shape.rotation.y += 0.01;
 	renderer.render( scene, camera );
 	requestAnimationFrame( render );
 }
